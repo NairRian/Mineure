7c43ceb42c1630116b76ba26c256c9e7681bbe39
NairRian <166063685+NairRian@users.noreply.github.com>
2024-11-28 09:46:42 +0100

    Update Volcans.py

--- Volcans.py
+++ Volcans.py
@@ -45,33 +45,30 @@ type_stats
 
 # afficher le graphique pour un binôme
 # demander le binôme
-#rock_type = input("Quel type de roche ? attention à respecter l'écriture correcte : ")
-#tectonic_setting = input("Quel cas tectonique ? attention à respecter l'écriture correcte : ")
-
 rock_type = st.selectbox("Quel type de roche ?", liste_rock)
 tectonic_setting = st.selectbox("Quel cas tectonique ?", liste_tecto)
 st.write(f"Vous avez choisi : {rock_type} et {tectonic_setting}")
 
 if (rock_type, tectonic_setting) in type_stats.index:
-  type_counts = type_stats.loc[(rock_type, tectonic_setting)] # loc permet de créer un tableau qui répertorie les types de roche et leur itération
-  type_counts = type_counts[type_counts != 0] # suppression des valeurs = 0
-  tot = sum(type_counts)
-  for i in type_counts.index:  # Parcourir les index
-    type_counts[i] = type_counts[i] / tot * 100
-
-  # Create a bar plot
-  plt.figure(figsize=(10, 6))
-  type_counts.plot(kind='bar')
-  plt.title(f"Distribution des types d'éruption pour la combinaison : {rock_type} & {tectonic_setting}")
-  plt.xlabel("Type d'éruption")
-  plt.xticks(rotation=45, ha='right')
-  plt.ylabel('Pourcentrage de distribution')
-  plt.ylim(0, 100)
-  plt.yticks(range(0,101,10))
-  plt.tight_layout()
-  plt.show()
+    type_counts = type_stats.loc[(rock_type, tectonic_setting)] # loc permet de créer un tableau qui répertorie les types de roche et leur itération
+    type_counts = type_counts[type_counts != 0] # suppression des valeurs = 0
+    tot = sum(type_counts)
+    for i in type_counts.index:  # Parcourir les index
+        type_counts[i] = type_counts[i] / tot * 100
+    
+    # Create a bar plot
+    plt.figure(figsize=(10, 6))
+    type_counts.plot(kind='bar')
+    plt.title(f"Distribution des types d'éruption pour la combinaison : {rock_type} & {tectonic_setting}")
+    plt.xlabel("Type d'éruption")
+    plt.xticks(rotation=45, ha='right')
+    plt.ylabel('Pourcentrage de distribution')
+    plt.ylim(0, 100)
+    plt.yticks(range(0,101,10))
+    plt.tight_layout()
+    plt.show()
 else:
-  print(f"La combinaison indiquée n'existe pas : {rock_type} & {tectonic_setting}")
+    print(f"La combinaison indiquée n'existe pas : {rock_type} & {tectonic_setting}")
 
 
 #st.dataframe(df.style.highlight_max(axis=0)) surligne la valeur max de la colonne
