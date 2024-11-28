363a136eeb91f93090212f392d751326715ab2d0
NairRian <166063685+NairRian@users.noreply.github.com>
2024-11-28 09:15:13 +0100

    Update Volcans.py

--- Volcans.py
+++ Volcans.py
@@ -5,15 +5,15 @@ import seaborn as sns
 import math
 from sklearn.datasets import load_wine
 
-"""
-values, target = load_wine(return_X_y=True)
-target_names = load_wine().target_names
-feature_names = load_wine().feature_names
-"""
+
+#values, target = load_wine(return_X_y=True)
+#target_names = load_wine().target_names
+#feature_names = load_wine().feature_names
+
 st.title("Les volcans de l'Holocène")
 
 # Chemin du fichier CSV local
-csv_path = "C:/Users/camph/Documents/2024-5/S7/database.csv"
+csv_path = "C:/Users/camph/Documents/Codage/database.csv"
 
 # Lecture du fichier CSV avec pandas
 try:
@@ -30,12 +30,11 @@ except FileNotFoundError:
 except Exception as e:
     st.error(f"Une erreur s'est produite : {e}")
 
-"""
-st.dataframe(df.style.highlight_max(axis=0)) surligne la valeur max de la colonne
-plot =df['alcohol']
 
-bot = st.checkbox("Afficher le graphique de la colonne alcohol : ")
-if bot :
-  st.write('Voici le graphique de la colonne alcohol : ')
-  st.line_chart(plot)
-"""
+#st.dataframe(df.style.highlight_max(axis=0)) surligne la valeur max de la colonne
+#plot =df['alcohol']
+
+#bot = st.checkbox("Afficher le graphique de la colonne alcohol : ")
+#if bot :
+#  st.write('Voici le graphique de la colonne alcohol : ')
+#  st.line_chart(plot)
