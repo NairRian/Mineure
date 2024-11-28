6e83a7525a212b8158c7eedcb870a7fcf5c123d0
NairRian <166063685+NairRian@users.noreply.github.com>
2024-11-28 08:47:00 +0100

    Create Volcans.py
    créer l'environnement avec un contenu faux

---
+++ Volcans.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+from sklearn.datasets import load_wine
+
+values, target = load_wine(return_X_y=True)
+target_names = load_wine().target_names
+feature_names = load_wine().feature_names
+
+st.title('Bonjour !')
+
+df = pd.DataFrame(values, columns=feature_names)
+df
+plot =df['alcohol']
+
+bot = st.checkbox("Afficher le graphique de la colonne alcohol : ")
+if bot :
+  st.write('Voici le graphique de la colonne alcohol : ')
+  st.line_chart(plot)
