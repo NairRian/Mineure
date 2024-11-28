e0cda14d2d6cbc5d791c8ae7ae48c8a42fe17719
NairRian <166063685+NairRian@users.noreply.github.com>
2024-11-28 08:52:17 +0100

    Update Volcans.py
    add libs
    new title
    modify dataframe

--- Volcans.py
+++ Volcans.py
@@ -1,15 +1,18 @@
-import streamlit as st
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
 from sklearn.datasets import load_wine
 
 values, target = load_wine(return_X_y=True)
 target_names = load_wine().target_names
 feature_names = load_wine().feature_names
 
-st.title('Bonjour !')
+st.title("Les volcans de l'Holocène")
 
 df = pd.DataFrame(values, columns=feature_names)
-df
+st.dataframe(df.style.highlight_max(axis=0))
 plot =df['alcohol']
 
 bot = st.checkbox("Afficher le graphique de la colonne alcohol : ")
