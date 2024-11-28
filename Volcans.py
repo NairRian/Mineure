2a3ba749913e7ca220ba34cf469f6f7af0c7534f
NairRian <166063685+NairRian@users.noreply.github.com>
2024-11-28 09:25:01 +0100

    Update Volcans.py
    add moitié code Raph

--- Volcans.py
+++ Volcans.py
@@ -3,32 +3,46 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import math
-from sklearn.datasets import load_wine
-
-
-#values, target = load_wine(return_X_y=True)
-#target_names = load_wine().target_names
-#feature_names = load_wine().feature_names
 
 st.title("Les volcans de l'Holocène")
+
+st.write("Exposer nos idées : ")
 
 # Chemin du fichier CSV local
 csv_path = "./database.csv"
 
 # Lecture du fichier CSV avec pandas
-try:
-    df = pd.read_csv(csv_path)
-    # Affichage des données dans l'application
-    st.write("Aperçu des données :")
-    st.dataframe(df)
+df = pd.read_csv(csv_path)
+# Affichage des données dans l'application
+st.write("Aperçu des données :")
+st.dataframe(df)
 
-    # Optionnel : ajouter des statistiques ou des graphiques
-    st.write("Statistiques descriptives :")
-    st.write(df.describe())
-except FileNotFoundError:
-    st.error(f"Le fichier '{csv_path}' est introuvable. Veuillez vérifier le chemin.")
-except Exception as e:
-    st.error(f"Une erreur s'est produite : {e}")
+# Optionnel : ajouter des statistiques ou des graphiques
+st.write("Statistiques descriptives :")
+st.write(df.describe())
+
+# afficher la version traitée du document
+st.write("Nous avons effectué les modifications suivantes : ")
+# dff = modif de df
+# st.dataframe(dff)
+
+# ANALYSE SELON TYPE DE ROCHES, D'ERUPTION ET DE TECTONIQUE
+# réduire l'excel aux colonnes qui nous intéressent
+data_infos = data[['Type', 'Dominant Rock Type', 'Tectonic Setting']]
+# créer une liste des éléments dans chaque colonne sans doublons
+liste_type = list(set(data_infos['Type'].values))
+liste_rock = list(set(data_infos['Dominant Rock Type'].values))
+liste_tecto = list(set(data_infos['Tectonic Setting'].values))
+# grouper la data par 'Dominant Rock Type' and 'Tectonic Setting', et compter les itérations de chaque 'Type', si un 'Type' n'est pas représenté, indiquer 0
+type_stats = (
+    data_infos.groupby(['Dominant Rock Type', 'Tectonic Setting'])['Type']
+    .value_counts() # compter les itérations
+    .rename_axis(['Dominant Rock Type', 'Tectonic Setting', 'Type'])  # Ajouter les intitulés
+    .unstack(fill_value=0)  # si valeur nulle
+)
+type_stats
+
+
 
 
 #st.dataframe(df.style.highlight_max(axis=0)) surligne la valeur max de la colonne
